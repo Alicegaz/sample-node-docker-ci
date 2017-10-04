69da85a91d69de53d4a14f6582e39cc8a1e734cb
AliceGaz <gazizullina2010@yandex.ru>
2017-10-04 15:10:40 +0300

    new

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-docker build -t kostyaurysov/sample-node .
-docker push kostyaurysov/sample-node
+docker build -t alicegaz/sample-node .
+docker push alicegaz/sample-node
 
-ssh deploy@35.187.30.81 << EOF
-docker pull kostyaurysov/sample-node:latest
+ssh -i ~/.ssh/my-ssh gazizullina143@104.198.135.103 << EOF
+docker pull alicegaz/sample-node:latest
 docker stop web || true
 docker rm web || true
-docker rmi kostyaurysov/sample-node:current || true
-docker tag kostyaurysov/sample-node:latest kostyaurysov/sample-node:current
-docker run -d --net app --restart always --name web -p 80:80 kostyaurysov/sample-node:current
+docker rmi alicegaz/sample-node:current || true
+docker tag alicegaz/sample-node:latest alicegaz/sample-node:current
+docker run -d --net app --restart always --name web -p 80:80 alicegaz/sample-node:current
 EOF
