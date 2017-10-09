803794c0ae05b6bd23e985e958503c00e2d068b3
AliceGaz <gazizullina2010@yandex.ru>
2017-10-09 19:12:40 +0300

    dfgds

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-docker push alicegaz/sample-node
+sudo docker push alicegaz/sample-node
 
 ssh gazizullina143@104.198.135.103 << EOF
-docker pull alicegaz/sample-node:latest
-docker stop web || true
-docker rm web || true
-docker rmi alicegaz/sample-node:current || true
-docker tag alicegaz/sample-node:latest alicegaz/sample-node:current
-docker run -d --restart always --name web -p 80:80 alicegaz/sample-node:current
+sudo docker pull alicegaz/sample-node:latest
+sudo docker stop web || true
+sudo docker rm web || true
+sudo docker rmi alicegaz/sample-node:current || true
+sudo docker tag alicegaz/sample-node:latest alicegaz/sample-node:current
+sudo docker run -d --restart always --name web -p 80:80 alicegaz/sample-node:current
 EOF
